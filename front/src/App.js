a2156823cd21592f29a8251dc1504116b0b1a9e6
indigoblake <indigoblakework@gmail.com>
2020-09-14 20:31:06 +1200

    Add Bulma

--- front/src/App.js
+++ front/src/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import Content from './Content';
+import Sidebar from './Sidebar';
 
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bulma/css/bulma.css';
 
 
 function App() {
-  return (
-    <div className="App">
-      <div className="row">
-          <div className="row">
-              <div className="col">                
-                <nav class="nav flex-column">
-                    <a class="nav-link active">Active</a>
-                    <a class="nav-link">Link</a>
-                    <a class="nav-link">Link</a>
-                    <a class="nav-link disabled">Disabled</a>
-                </nav>
-              </div>
-          </div>
-        <div className="col">
-            <Content />
+    return (
+        <div className="App">
+            <div className="section">
+                <div className="columns">
+                    <div class="column is-one-quarter">
+                        <Sidebar />
+                    </div>
+                    <div className="column">
+                        <Content />
+                    </div>
+                </div>
+            </div>
+
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
