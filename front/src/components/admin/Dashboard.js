28b1578ee1b0431ebf97e40f05a1cd928a95d2c2
indigoblake <indigoblakework@gmail.com>
2020-09-27 21:14:11 +1300

    Add test styles
    To check how it looks on Mac

--- front/src/components/admin/Dashboard.js
+++ front/src/components/admin/Dashboard.js
@@ -2,9 +2,11 @@ import React, { useEffect, useContext } from 'react'
 import { Summary } from '../../elements/Summary'
 import { Employees } from '../../elements/Employees'
 import { EmployeeContext } from '../../contexts/EmployeeContext'
+import { Entries } from '../../elements/Entries'
 
 export const Dashboard = () => {
   const { updateEmployees } = useContext(EmployeeContext);
+
   useEffect(() => {
     fetch('http://localhost:5000/api/employees/merged')
       .then(res => res.json())
@@ -33,6 +35,7 @@ export const Dashboard = () => {
           </div>
         </div>
       </section>
+      <Entries />
       <Summary />
       <Employees />
     </>
---
+++ front/src/elements/Entries.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export const Entries = () => {
+  return (
+    <div>
+      <div className="entries box is-absolute">
+        <table class="table w-100">
+          <thead>
+            <tr>
+              <th>Team</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>fsfsdf</td>
+            </tr>
+            <tr>
+              <td>fsfsdf</td>
+            </tr>
+            <tr>
+              <td>fsfsdf</td>
+            </tr>
+            <tr>
+              <td>fsfsdf</td>
+            </tr>
+            <tr>
+              <td>fsfsdf</td>
+            </tr>
+            <tr>
+              <td>fsfsdf</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
